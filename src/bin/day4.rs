4745d2a17de22970e23a7a5c9b4579f85cf055f0
Alessandro Fulgini <fuljo97@gmail.com>
2022-12-04 18:25:53 +0100

    Day 4 (part 1)

---
+++ src/bin/day4.rs
@@ -0,0 +1,20 @@
+use std::{fs::{File}, io::{BufReader, BufRead}};
+
+use scanf::sscanf;
+
+fn main() {
+    let f = File::open("input/day4.txt").unwrap();
+    let read = BufReader::new(f);
+
+    let tot_overlap: usize = read.lines()
+    .map(|l| l.unwrap())
+    .filter(|l| {
+        let mut s1: usize = 0;
+        let mut s2: usize = 0;
+        let mut e1: usize = 0;
+        let mut e2: usize = 0;
+        sscanf!(l, "{}-{},{}-{}", s1, e1, s2, e2).expect("scanf");
+        (s1 <= s2 && e2 <= e1) || (s2 <= s1 && e1 <= e2)  
+    }).count();
+    println!("{}", tot_overlap);
+}
